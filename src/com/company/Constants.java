425fbdab856d4fea666d61c8413543ba8f174cea
zhangjiyun <ayorfree@gmail.com>
2017-03-02 16:47:18 +0800

    FirstSample & Constants-1

---
+++ src/com/company/Constants.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * Created by zhangjiyun on 2017/3/2.
+ */
+public class Constants
+{
+    public static void main(String[] args)
+    {
+        final double CM_PER_INCH = 2.54;
+        double paperWidth = 8.5;
+        double paperHeight = 11;
+        System.out.print("Paper size in centimeters:" + paperWidth * CM_PER_INCH + " by" + paperHeight * CM_PER_INCH);
+    }
+}
---
+++ src/com/company/FirstSample.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by zhangjiyun on 2017/3/2.
+ */
+public class FirstSample
+{
+    public static void main(String[] args)
+    {
+        System.out.println("My Nanme Is Zhangjiyun.");
+    }
+}
