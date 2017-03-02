478110f6c54da867a9fd8a16d7f8002362acdd21
zhangjiyun <ayorfree@gmail.com>
2017-03-02 21:55:27 +0800

    ooo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,24 @@
 package com.company;
 
+import static java.lang.System.out;
+
 public class Main {
 
     public static void main(String[] args)
     {
-        //
+        // Math.sin()
+        // Math.cos
+
+        double x = 4;
+        double y = Math.sqrt(x);
+
+        double z = 9.996;
+        int nz = (int) z;
+        int oz = (int) Math.round(z);
+
+        System.out.println(y + Math.PI + Math.E);
+        out.println(nz);
+        out.println(oz);
     }
 }
 
