dc23a49bec92ed2b7a207c538d39733fec1641f4
zhangjiyun <ayorfree@gmail.com>
2017-03-05 21:40:49 +0800

    class & object day one

---
+++ src/com/company/EmployeeTest.java
@@ -0,0 +1,58 @@
+package com.company;
+
+import java.util.*;
+/**
+ * Created by zhangjiyun on 2017/3/5.
+ */
+public class EmployeeTest {
+    public static void main(String[] args)
+    {
+        Employee[] staff = new Employee[3];
+        staff[0] = new Employee("张继云", 87000,1988, 2, 21);
+        staff[1] = new Employee("刘文", 85000,1988, 3, 21);
+        staff[2] = new Employee("马可吐", 78000,1991, 4, 19);
+
+        for (Employee e :
+                staff)
+            e.raiseSalary(5);
+
+        for (Employee e :
+                staff)
+            System.out.println("name=" + e.getName() + ", salary="  + e.getSalary() +   ", hireDay="  + e.getHireDay());
+    }
+}
+
+    class Employee
+    {
+        private String name;
+        private double salary;
+        private Date hireDay;
+
+        public Employee(String n, double s, int year, int month, int day)
+        {
+            name = n;
+            salary = s;
+
+            GregorianCalendar calendar = new GregorianCalendar(year, month - 1, day);
+            hireDay = calendar.getTime();
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public double getSalary() {
+            return salary;
+        }
+
+        public Date getHireDay() {
+            return hireDay;
+        }
+
+        public void raiseSalary(double byPercent)
+        {
+            double raise = salary * byPercent / 100;
+            salary += raise;
+        }
+    }
