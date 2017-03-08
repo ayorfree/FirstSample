bc4844f18960b37c91185be61d357d468b75b188
zhangjiyun <ayorfree@gmail.com>
2017-03-08 16:18:55 +0800

    2017.03.08

---
+++ src/com/starfucker_inc/v1ch05/inheritance/Employee.java
@@ -0,0 +1,43 @@
+package com.starfucker_inc.v1ch05.inheritance;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * Created by zhangjiyun on 2017/3/8.
+ */
+class Employee
+{
+    private String name;
+    private double salary;
+    private Date hireDay;
+
+    public Employee(String n, double s, int year, int month, int day)
+    {
+        name = n;
+        salary = s;
+        GregorianCalendar calendar = new GregorianCalendar(year, month, day);
+        hireDay = calendar.getTime();
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public double getSalary()
+    {
+        return salary;
+    }
+
+    public Date getHireDay()
+    {
+        return hireDay;
+    }
+
+    public void raiseSalary(double byPercent)
+    {
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
+}
