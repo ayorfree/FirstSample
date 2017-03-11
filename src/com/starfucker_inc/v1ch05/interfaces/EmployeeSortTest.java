ae2e8566569638455dcc54b64dc89966f1f9754b
zhangjiyun <ayorfree@gmail.com>
2017-03-11 15:30:07 +0800

    2017.03.11-3

--- src/com/starfucker_inc/v1ch05/interfaces/EmployeeSortTest.java
+++ src/com/starfucker_inc/v1ch05/interfaces/EmployeeSortTest.java
@@ -6,20 +6,21 @@ import java.util.Arrays;
  * Created by zhangjiyun on 2017/3/11.
  */
 
+
 public class EmployeeSortTest
 {
     public static void main(String[] args)
     {
         Employee[] staff = new Employee[3];
-        staff[0] = new Employee("lucy",30000);
-        staff[1] = new Employee("lily",35000);
-        staff[2] = new Employee("cat",28000);
+        staff[0] = new Employee("lily", 30000);
+        staff[1] = new Employee("lucy",35000);
+        staff[2] = new Employee("bill",28000);
 
         Arrays.sort(staff);
 
         for (Employee e :
                 staff) {
-            System.out.println("name=" + e.getName() +",salary=" + e.getSalary());
+            System.out.println("name=" + e.getName() + ",salary=" + e.getSalary());
         }
     }
 }
