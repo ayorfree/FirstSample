dac8d0c394d42d49450a2e320a9b6b23ef171d78
zhangjiyun <ayorfree@gmail.com>
2017-03-13 17:59:45 +0800

    2017.03.13-revised

--- src/com/starfucker_inc/v2ch01/Welcome.java
+++ src/com/starfucker_inc/v2ch01/Welcome.java
@@ -16,7 +16,6 @@ public class Welcome
                 s) {
             System.out.print(f);
         }
-
     }
 }
 
---
+++ src/com/testing/just_testing/InputTest.java
@@ -0,0 +1,20 @@
+package com.testing.just_testing;
+
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+import java.io.*;
+
+/**
+ * Created by zhangjiyun on 2017/3/13.
+ */
+public class InputTest
+{
+    public static void main(String[] args)
+    {
+        Locale.setDefault(Locale.US);
+        System.out.printf("%tc\n", new Date());
+        System.out.printf("%1$s %2$tB %2$te, %2$ty\n", "Due Date:", new Date());
+        System.out.printf("%s %tB %<te, %<ty", "Due Date:", new Date());
+    }
+}
