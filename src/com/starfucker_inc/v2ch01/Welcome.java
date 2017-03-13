67d5ada7d04cd806755cae8412341bd3bffeeefe
zhangjiyun <ayorfree@gmail.com>
2017-03-13 16:36:12 +0800

    2017.03.13-fwordwow

---
+++ src/com/starfucker_inc/v2ch01/Welcome.java
@@ -0,0 +1,22 @@
+package com.starfucker_inc.v2ch01;
+
+/**
+ * Created by zhangjiyun on 2017/3/13.
+ */
+public class Welcome
+{
+    public static void main(String[] args)
+    {
+        String[] s = new String[3];
+        s[0] = "Fuck me";
+        s[1] = " from";
+        s[2] = " behind.";
+
+        for (String f :
+                s) {
+            System.out.print(f);
+        }
+
+    }
+}
+
