560aae706d162674faab192745e6dac4f8bb4cbd
zhangjiyun <ayorfree@gmail.com>
2017-03-15 15:59:34 +0800

    2017.03.15-stack trace v2

--- src/com/starfucker_inc/v1ch11/StackTraceTest.java
+++ src/com/starfucker_inc/v1ch11/StackTraceTest.java
@@ -3,24 +3,24 @@ package com.starfucker_inc.v1ch11;
 import java.util.Scanner;
 
 /**
- * Created by zhangjiyun on 2017/3/15.
+ * This program demonstrates stacktrace
+ * @version 2017.3.15
+ * @author ayorfree
  */
-
 public class StackTraceTest
 {
     public static int factorial(int n)
     {
         System.out.println("factorial(" + n +"):");
+
         Throwable t = new Throwable();
         StackTraceElement[] frames = t.getStackTrace();
-        for (StackTraceElement f :
-                frames) {
-            System.out.println(f);
+        for (StackTraceElement f: frames) {System.out.println(f);
         }
 
         int r;
         if (n <= 1) r = 1;
-        else r = n * factorial(n - 1);
+        else r = factorial(n - 1) * n;
         System.out.println("return: " + r);
         return r;
     }
@@ -28,6 +28,7 @@ public class StackTraceTest
     public static void main(String[] args)
     {
         System.out.println("Enter n: ");
+
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         factorial(n);
