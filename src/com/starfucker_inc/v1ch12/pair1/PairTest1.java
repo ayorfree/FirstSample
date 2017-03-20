cc90545c2f2b84c8a464d2a05c612224f8d3dbe1
zhangjiyun <ayorfree@gmail.com>
2017-03-20 21:56:28 +0800

    2017.03.16-generic3

--- src/com/starfucker_inc/v1ch12/pair1/PairTest1.java
+++ src/com/starfucker_inc/v1ch12/pair1/PairTest1.java
@@ -9,20 +9,20 @@ public class PairTest1
 {
     public static void main(String[] args)
     {
-        String[] sick = {"Papa", "had", "a", "new", "band"};
-        Pair<String> mm = ArrayAlg.minmax(sick);
-        System.out.println("min = " + mm.getFirst());
-        System.out.println("max = " + mm.getSecond());
+        String[] coil = {"in", "my", "blood", "it", "is", "soul"};
+        Pair<String> ape = ArraysAlg.minmax(coil);
+        System.out.println("min = " + ape.getFirst());
+        System.out.println("max = " + ape.getSecond());
     }
 }
 
-class ArrayAlg
+class ArraysAlg
 {
     public static Pair<String> minmax(String[] a)
     {
         if (a == null || a.length == 0) return null;
-        String max = a[0];
         String min = a[0];
+        String max = a[0];
         for (int i = 1; i < a.length; i++) {
             if (min.compareTo(a[i]) > 0) min = a[i];
             if (max.compareTo(a[i]) < 0) max = a[i];
@@ -50,15 +50,5 @@ class Pair<T>
     public T getSecond()
     {
         return second;
-    }
-
-    public void setFirst(T first)
-    {
-        this.first = first;
-    }
-
-    public void setSecond(T second)
-    {
-        this.second = second;
     }
 }
