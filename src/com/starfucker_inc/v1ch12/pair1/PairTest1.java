caf046cb274c8b9304377ea346cc7757506d6013
zhangjiyun <ayorfree@gmail.com>
2017-03-20 21:42:33 +0800

    2017.03.16-generic2

--- src/com/starfucker_inc/v1ch12/pair1/PairTest1.java
+++ src/com/starfucker_inc/v1ch12/pair1/PairTest1.java
@@ -1,17 +1,16 @@
 package com.starfucker_inc.v1ch12.pair1;
 
-import com.starfucker_inc.v1ch10.systemInfo.SystemInfo;
-
 /**
- * @version 1
+ * @version 2017.3.20
  * @author ayor
  */
+
 public class PairTest1
 {
     public static void main(String[] args)
     {
-        String[] words = {"Mary", "had", "a", "little" ,"lamb"};
-        Pair<String> mm = ArrayAlg.minmax(words);
+        String[] sick = {"Papa", "had", "a", "new", "band"};
+        Pair<String> mm = ArrayAlg.minmax(sick);
         System.out.println("min = " + mm.getFirst());
         System.out.println("max = " + mm.getSecond());
     }
@@ -22,9 +21,9 @@ class ArrayAlg
     public static Pair<String> minmax(String[] a)
     {
         if (a == null || a.length == 0) return null;
-        String min = a[0];
         String max = a[0];
-        for (int i = 0; i < a.length; i++) {
+        String min = a[0];
+        for (int i = 1; i < a.length; i++) {
             if (min.compareTo(a[i]) > 0) min = a[i];
             if (max.compareTo(a[i]) < 0) max = a[i];
         }
@@ -37,26 +36,29 @@ class Pair<T>
     private T first;
     private T second;
 
-    public Pair(){first = null; second = null;};
     public Pair(T first, T second)
     {
         this.first = first;
         this.second = second;
     }
 
-    public T getFirst(){
+    public T getFirst()
+    {
         return first;
     }
 
-    public T getSecond(){
+    public T getSecond()
+    {
         return second;
     }
 
-    public void setFirst(T first){
+    public void setFirst(T first)
+    {
         this.first = first;
     }
 
-    public void setSecond(T second){
+    public void setSecond(T second)
+    {
         this.second = second;
     }
 }
