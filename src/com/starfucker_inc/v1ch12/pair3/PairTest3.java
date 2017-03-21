46940ed1fc8e8f3fdb979e8142b4e91c20837e90
zhangjiyun <ayorfree@gmail.com>
2017-03-21 23:52:16 +0800

    2017.03.16-generic/cc1

--- src/com/starfucker_inc/v1ch12/pair3/PairTest3.java
+++ src/com/starfucker_inc/v1ch12/pair3/PairTest3.java
@@ -1,36 +1,41 @@
 package com.starfucker_inc.v1ch12.pair3;
 
-import java.util.*;
+
+import java.util.GregorianCalendar;
 
 /**
- * @version test
+ * @version 2017.3.20
  * @author ayor
  */
-public class PairTest3 {
+
+public class PairTest3
+{
     public static void main(String[] args)
     {
-        Manager ceo = new Manager("Gus Greedy", 80000, 2003, 12, 15);
-        Manager cfo = new Manager("Sid Sneaky", 60000, 2003, 12, 15);
-        Pair<Manager>  buddies = new Pair<>(ceo, cfo);
-        printBuddies(buddies);
+        Manager ceo = new Manager("billy cogen" , 100000, 1976,4,2);
+        Manager cfo = new Manager("thom yorke", 100000, 1976, 4, 2);
+        Pair<Manager> boddies = new Pair<>(ceo, cfo);
+        printBoddies(boddies);
 
-        ceo.setBonus(1000000);
-        cfo.setBonus(500000);
+        ceo.setBonus(100000);
+        cfo.setBonus(90000);
+
         Manager[] managers = {ceo, cfo};
 
         Pair<Employee> result = new Pair<>();
         minmaxBonus(managers, result);
-        System.out.println("first: " + result.getFirst().getName() + ", second: " + result.getSecond().getName());
+        System.out.println("first: " + result.getFirst().getName() + ", and second: " + result.getSecond().getName());
+
         maxminBonus(managers, result);
-        System.out.println("first: " + result.getFirst().getName() + ", second: " + result.getSecond().getName());
+        System.out.println("first: " + result.getFirst().getName() + ", and second: " + result.getSecond().getName());
 
     }
 
-    public static void printBuddies(Pair<? extends Employee> p)
+    public static void printBoddies(Pair<? extends Employee> p)
     {
         Employee first = p.getFirst();
         Employee second = p.getSecond();
-        System.out.println(first.getName() + " and " + second.getName() + " are buddies.");
+        System.out.println(first.getName() + " and " + second.getName() + " are boddies.");
     }
 
     public static void minmaxBonus(Manager[] a, Pair<? super Manager> result)
@@ -38,7 +43,7 @@ public class PairTest3 {
         if (a == null || a.length == 0) return;
         Manager min = a[0];
         Manager max = a[0];
-        for (int i = 1; i < a.length; i++) {
+        for (int i = 1; i < a.length ; i++) {
             if (min.getBonus() > a[i].getBonus()) min = a[i];
             if (max.getBonus() < a[i].getBonus()) max = a[i];
         }
@@ -49,13 +54,13 @@ public class PairTest3 {
     public static void maxminBonus(Manager[] a, Pair<? super Manager> result)
     {
         minmaxBonus(a, result);
-        PairAlg.swapHelper(result);
+        ArrayAlg.swapHelper(result);
     }
 }
 
-class PairAlg
+class ArrayAlg
 {
-    public static boolean hasNulls(Pair<?> p)
+    public static boolean hasNull(Pair<?> p)
     {
         return p.getFirst() == null || p.getSecond() == null;
     }
@@ -69,7 +74,7 @@ class PairAlg
     {
         T t = p.getFirst();
         p.setSecond(p.getFirst());
-        p.setSecond(t);
+        p.setFirst(t);
     }
 }
 
@@ -79,8 +84,8 @@ class Pair<T>
     private T second;
 
     public Pair(){
-        this.first = null;
-        this.second = null;
+        first = null;
+        second = null;
     }
 
     public Pair(T first, T second)
@@ -89,19 +94,23 @@ class Pair<T>
         this.second = second;
     }
 
-    public T getFirst(){
+    public T getFirst()
+    {
         return first;
     }
 
-    public T getSecond(){
+    public T getSecond()
+    {
         return second;
     }
 
-    public void setFirst(T first){
+    public void setFirst(T first)
+    {
         this.first = first;
     }
 
-    public void setSecond(T second){
+    public void setSecond(T second)
+    {
         this.second = second;
     }
 }
@@ -110,7 +119,7 @@ class Manager extends Employee
 {
     private double bonus;
 
-    public Manager(String n, double s, int year, int month, int day)
+    public Manager(String n, double s, int year, int month ,int day)
     {
         super(n, s, year, month, day);
         bonus = 0;
@@ -126,6 +135,7 @@ class Manager extends Employee
         bonus = b;
     }
 }
+
 class Employee
 {
     private String name;
@@ -135,8 +145,7 @@ class Employee
     {
         name = n;
         salary = s;
-        new GregorianCalendar(year, month -1 , day).getTime();
-
+        new GregorianCalendar(year, month - 1, day).getTime();
     }
 
     public String getName()
--- src/com/testing/just_testing/MultiArrays.java
+++ src/com/testing/just_testing/MultiArrays.java
@@ -1,9 +1,52 @@
 package com.testing.just_testing;
 
-import java.util.Arrays;
-import java.util.Scanner;
 
 /**
- * Created by zhangjiyun on 2017/3/14.
+ * just testing for inheritance.
  */
 
+public class MultiArrays
+{
+    public static void main(String[] args)
+    {
+        Manager lee = new Manager("lee an la");
+        Employee ceo = lee;
+        lee.setBonus(50000);
+        System.out.println("my name is " + lee.getName() + ", and my bonus is " + lee.getBonus());
+    }
+}
+
+class Manager extends Employee
+{
+    private double bonus;
+
+    public Manager(String n)
+    {
+        super(n);
+        bonus = 0;
+    }
+
+    public void setBonus(double b)
+    {
+        bonus = b;
+    }
+
+    public double getBonus()
+    {
+        return bonus;
+    }
+}
+class Employee
+{
+    private String name;
+
+    public Employee(String n)
+    {
+        name = n;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+}
