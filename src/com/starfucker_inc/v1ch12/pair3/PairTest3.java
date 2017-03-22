753dcc135814353402b43f52994601cdc75a608c
zhangjiyun <ayorfree@gmail.com>
2017-03-22 09:19:45 +0800

    2017.03.16-generic/cc3

--- src/com/starfucker_inc/v1ch12/pair3/PairTest3.java
+++ src/com/starfucker_inc/v1ch12/pair3/PairTest3.java
@@ -1,41 +1,34 @@
 package com.starfucker_inc.v1ch12.pair3;
 
-
+import java.util.Date;
 import java.util.GregorianCalendar;
-
-/**
- * @version 2017.3.20
- * @author ayor
- */
 
 public class PairTest3
 {
     public static void main(String[] args)
     {
-        Manager ceo = new Manager("billy cogen" , 100000, 1976,4,2);
-        Manager cfo = new Manager("thom yorke", 100000, 1976, 4, 2);
-        Pair<Manager> boddies = new Pair<>(ceo, cfo);
-        printBoddies(boddies);
+        Manager ceo = new Manager("billy corgan" , 500000, 1987,3,4);
+        Manager cfo = new Manager("thom yorke", 500000, 1987, 3, 4);
 
-        ceo.setBonus(100000);
-        cfo.setBonus(90000);
+        ceo.setBonus(1000000);
+        cfo.setBonus(900000);
+
+        Pair<Manager> buddies = new Pair<>(ceo, cfo);
+        printBoddies(buddies);
 
         Manager[] managers = {ceo, cfo};
-
         Pair<Employee> result = new Pair<>();
         minmaxBonus(managers, result);
         System.out.println("first: " + result.getFirst().getName() + ", and second: " + result.getSecond().getName());
-
         maxminBonus(managers, result);
         System.out.println("first: " + result.getFirst().getName() + ", and second: " + result.getSecond().getName());
-
     }
 
     public static void printBoddies(Pair<? extends Employee> p)
     {
         Employee first = p.getFirst();
         Employee second = p.getSecond();
-        System.out.println(first.getName() + " and " + second.getName() + " are boddies.");
+        System.out.println(first.getName() + " and " + second.getName() + " are buddies.");
     }
 
     public static void minmaxBonus(Manager[] a, Pair<? super Manager> result)
@@ -43,7 +36,8 @@ public class PairTest3
         if (a == null || a.length == 0) return;
         Manager min = a[0];
         Manager max = a[0];
-        for (int i = 1; i < a.length ; i++) {
+
+        for (int i = 1; i < a.length; i++) {
             if (min.getBonus() > a[i].getBonus()) min = a[i];
             if (max.getBonus() < a[i].getBonus()) max = a[i];
         }
@@ -54,13 +48,13 @@ public class PairTest3
     public static void maxminBonus(Manager[] a, Pair<? super Manager> result)
     {
         minmaxBonus(a, result);
-        ArrayAlg.swapHelper(result);
+        ArrayAlg.swap(result);
     }
 }
 
 class ArrayAlg
 {
-    public static boolean hasNull(Pair<?> p)
+    public static boolean hasNulls(Pair<?> p)
     {
         return p.getFirst() == null || p.getSecond() == null;
     }
@@ -72,7 +66,7 @@ class ArrayAlg
 
     public static <T> void swapHelper(Pair<T> p)
     {
-        T t = p.getFirst();
+        T t = p.getSecond();
         p.setSecond(p.getFirst());
         p.setFirst(t);
     }
@@ -80,8 +74,8 @@ class ArrayAlg
 
 class Pair<T>
 {
-    private T first;
-    private T second;
+    T first;
+    T second;
 
     public Pair(){
         first = null;
@@ -119,7 +113,7 @@ class Manager extends Employee
 {
     private double bonus;
 
-    public Manager(String n, double s, int year, int month ,int day)
+    public Manager(String n, double s, int year, int month, int day)
     {
         super(n, s, year, month, day);
         bonus = 0;
@@ -134,18 +128,22 @@ class Manager extends Employee
     {
         bonus = b;
     }
+
+
 }
 
 class Employee
 {
     private String name;
     private double salary;
+    private Date hireDay;
 
     public Employee(String n, double s, int year, int month, int day)
     {
         name = n;
         salary = s;
-        new GregorianCalendar(year, month - 1, day).getTime();
+        GregorianCalendar calendar = new GregorianCalendar(year, month - 1, day);
+        hireDay = calendar.getTime();
     }
 
     public String getName()
@@ -156,5 +154,10 @@ class Employee
     public double getSalary()
     {
         return salary;
+    }
+
+    public Date getHireDay()
+    {
+        return hireDay;
     }
 }
