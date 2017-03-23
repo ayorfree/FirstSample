d9f2ef114f70895c0133585a65e75e64c3cdddc5
zhangjiyun <ayorfree@gmail.com>
2017-03-23 10:29:37 +0800

    collectiontest-1-linklisttest1

--- src/com/starfucker_inc/v1ch13/linkedList/LinkedListTest.java
+++ src/com/starfucker_inc/v1ch13/linkedList/LinkedListTest.java
@@ -6,33 +6,36 @@ import java.util.List;
 import java.util.ListIterator;
 
 /**
- * @version 2017.3.23
+ * this program demonstrate how to operate linkedlist premiliary.
+ * @version 3.23
  * @author ayorfree
  */
+
 public class LinkedListTest
 {
     public static void main(String[] args)
     {
         List<String> a = new LinkedList<>();
-        a.add("Amy");
-        a.add("Carl");
+        a.add("Alice");
+        a.add("Caro");
         a.add("Erica");
+        a.add("Gloria");
 
         List<String> b = new LinkedList<>();
         b.add("Bob");
-        b.add("Doug");
-        b.add("France");
-        b.add("Gloria");
+        b.add("Dick");
+        b.add("Flour");
+        b.add("Hippo");
 
         ListIterator<String> aIter = a.listIterator();
         Iterator<String> bIter = b.iterator();
 
         while (bIter.hasNext())
         {
-            if (aIter.hasNext()) aIter.next();
-            aIter.add(bIter.next());
+           if (aIter.hasNext())
+               aIter.next();
+           aIter.add(bIter.next());
         }
-
         System.out.println(a);
 
         bIter = b.iterator();
@@ -45,7 +48,6 @@ public class LinkedListTest
                 bIter.remove();
             }
         }
-
         System.out.println(b);
 
         a.removeAll(b);
