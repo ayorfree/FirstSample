4a56f73fddea8d2c8cc6b6867ffa2fb108431ff0
zhangjiyun <ayorfree@gmail.com>
2017-03-23 09:16:37 +0800

    collectiontest-1-linklisttest

---
+++ src/com/starfucker_inc/v1ch13/linkedList/LinkedListTest.java
@@ -0,0 +1,54 @@
+package com.starfucker_inc.v1ch13.linkedList;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * @version 2017.3.23
+ * @author ayorfree
+ */
+public class LinkedListTest
+{
+    public static void main(String[] args)
+    {
+        List<String> a = new LinkedList<>();
+        a.add("Amy");
+        a.add("Carl");
+        a.add("Erica");
+
+        List<String> b = new LinkedList<>();
+        b.add("Bob");
+        b.add("Doug");
+        b.add("France");
+        b.add("Gloria");
+
+        ListIterator<String> aIter = a.listIterator();
+        Iterator<String> bIter = b.iterator();
+
+        while (bIter.hasNext())
+        {
+            if (aIter.hasNext()) aIter.next();
+            aIter.add(bIter.next());
+        }
+
+        System.out.println(a);
+
+        bIter = b.iterator();
+        while (bIter.hasNext())
+        {
+            bIter.next();
+            if (bIter.hasNext())
+            {
+                bIter.next();
+                bIter.remove();
+            }
+        }
+
+        System.out.println(b);
+
+        a.removeAll(b);
+        System.out.println(a);
+    }
+}
