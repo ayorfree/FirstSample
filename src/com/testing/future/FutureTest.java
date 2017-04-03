439105fae21d19438c0a3bfbddd6882ff3d66ff1
zhangjiyun <ayorfree@gmail.com>
2017-04-03 17:48:09 +0800

    future test

--- src/com/testing/future/FutureTest.java
+++ src/com/testing/future/FutureTest.java
@@ -1,122 +1,100 @@
 package com.testing.future;
 
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.concurrent.*;
 
 /**
- * @version 1.01 2012-01-26
- * @author Cay Horstmann
+ * @author ayorfree
+ * @create 2017-04-03-下午4:59
  */
-public class FutureTest
-{
-    public static void main(String[] args)
-    {
+
+public class FutureTest {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter base directory (e.g. /usr/local/jdk5.0/src): ");
+        System.out.print("Enter your directory: ");
         String directory = in.nextLine();
-        System.out.print("Enter keyword (e.g. volatile): ");
+        System.out.print("Enter your keyword: ");
         String keyword = in.nextLine();
 
         MatchCounter counter = new MatchCounter(new File(directory), keyword);
         FutureTask<Integer> task = new FutureTask<>(counter);
         Thread t = new Thread(task);
         t.start();
-        try
-        {
-            System.out.println(task.get() + " matching files.");
-        }
-        catch (ExecutionException e)
-        {
-            e.printStackTrace();
-        }
-        catch (InterruptedException e)
-        {
-        }
+
+       try {
+           System.out.println(task.get() + "matching files");
+       }
+       catch (ExecutionException e){
+           e.printStackTrace();
+       }
+       catch (InterruptedException e){
+       }
     }
 }
 
-/**
- * This task counts the files in a directory and its subdirectories that contain a given keyword.
- */
-class MatchCounter implements Callable<Integer>
-{
-    private File directory;
+class MatchCounter implements Callable<Integer>{
+    private File direcotry;
     private String keyword;
     private int count;
 
-    /**
-     * Constructs a MatchCounter.
-     * @param directory the directory in which to start the search
-     * @param keyword the keyword to look for
-     */
-    public MatchCounter(File directory, String keyword)
-    {
-        this.directory = directory;
+    public MatchCounter(File direcotry, String keyword ) {
+        this.direcotry = direcotry;
         this.keyword = keyword;
     }
 
-    public Integer call()
-    {
+    public Integer call(){
         count = 0;
-        try
-        {
-            File[] files = directory.listFiles();
-            List<Future<Integer>> results = new ArrayList<>();
-
-            for (File file : files)
-                if (file.isDirectory())
-                {
+        ArrayList<Future<Integer>> results = new ArrayList<>();
+        try {
+            File[] files = direcotry.listFiles();
+            for (File file :
+                    files) {
+                if (file.isDirectory()){
                     MatchCounter counter = new MatchCounter(file, keyword);
                     FutureTask<Integer> task = new FutureTask<>(counter);
                     results.add(task);
                     Thread t = new Thread(task);
                     t.start();
                 }
-                else
-                {
+                else {
                     if (search(file)) count++;
                 }
-
-            for (Future<Integer> result : results)
-                try
-                {
+            }
+            for (Future<Integer> result :
+                    results) {
+                try {
                     count += result.get();
                 }
-                catch (ExecutionException e)
-                {
+                catch (ExecutionException e) {
                     e.printStackTrace();
                 }
-        }
-        catch (InterruptedException e)
-        {
-        }
-        return count;
-    }
-
-    /**
-     * Searches a file for a given keyword.
-     * @param file the file to search
-     * @return true if the keyword is contained in the file
-     */
-    public boolean search(File file)
-    {
-        try
-        {
-            try (Scanner in = new Scanner(file))
-            {
-                boolean found = false;
-                while (!found && in.hasNextLine())
-                {
-                    String line = in.nextLine();
-                    if (line.contains(keyword)) found = true;
-                }
-                return found;
             }
+
+            }
+            catch (InterruptedException e) {
+            }
+            return count;
         }
-        catch (IOException e)
-        {
+
+    public boolean search(File file){
+        try {
+            try (Scanner in = new Scanner(file)){
+                boolean found = false;
+                while (!found && in.hasNextLine()){
+                    String line = in.nextLine();
+                    if (line.contains(keyword))
+                        found = true;
+                    }
+                    return found;
+                }
+            }
+            catch (IOException e){
             return false;
         }
     }
 }
+
+
