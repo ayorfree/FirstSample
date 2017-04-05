b677e81228dd4167b96b85821213426b4222f280
zhangjiyun <ayorfree@gmail.com>
2017-04-05 11:42:31 +0800

    w3c html-1

--- src/com/testing/just_testing/StringTest.java
+++ src/com/testing/just_testing/StringTest.java
@@ -1,34 +1,34 @@
 package com.testing.just_testing;
 
+import java.lang.reflect.Array;
+
 /**
  * Created by zhangjiyun on 2017/3/13.
  */
 public class StringTest
 {
-    public static void main(String[] args)
-    {
-        String coil = "the ape of naples";
-        String coil1 = " the ape of naples ";
+    public static void main(String[] args) {
         String coil3 = "theapeofnaples";
-        String coil4 = "is a great album.";
-        String coil5 = "Coil";
-        String con = coil + " " + coil4;
-        String upper = coil.toUpperCase();
-        String lower = upper.toLowerCase();
-        String acoil = coil5.toUpperCase();
-        int r = coil.compareTo(coil1);
 
-        System.out.println(con);
-        System.out.println(upper);
-        System.out.println(lower);
-        System.out.println(acoil);
-        System.out.println(coil1.trim());
-        System.out.println(con.length());
-        System.out.println(coil.charAt(1));
-        System.out.println(r);
-        System.out.println(coil1.offsetByCodePoints(1,7));
-        System.out.println(coil.equals(coil1));
-        System.out.println(coil.indexOf(coil1));
-        System.out.println(coil3.substring(3,6));
+        System.out.println(coil3.length());
+        System.out.println(coil3.charAt(0));
+        System.out.println(coil3.toUpperCase());
+
+        String r = revsor(coil3);
+
+        System.out.println(r.toUpperCase());
     }
+
+    private static String revsor(String s){
+        int n = s.length();
+        StringBuilder seq = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            seq.append(s.charAt(n - 1 - i));
+        }
+        return seq.toString();
+    }
+
 }
+
+
