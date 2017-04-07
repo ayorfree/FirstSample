f97b8bce20c583b296a90ce8c3c8064a534f54b9
zhangjiyun <ayorfree@gmail.com>
2017-04-07 14:30:51 +0800

    html test again

--- src/com/testing/future/FutureTest.java
+++ src/com/testing/future/FutureTest.java
@@ -8,7 +8,6 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
-import java.util.regex.Matcher;
 
 /**
  * @author ayorfree
