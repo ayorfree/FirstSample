b783b512366f7b632da06a9757f88887b633cadf
zhangjiyun <ayorfree@gmail.com>
2017-04-27 23:47:17 +0800

    sleepwalkers-1

--- src/com/testing/just_testing/Sleepwalkers.java
+++ src/com/testing/just_testing/Sleepwalkers.java
@@ -4,46 +4,24 @@ import java.util.Scanner;
 
 /**
  * david sylvian inspiration
- *
  * @author ayorfree
  * @create 2017-04-26-下午10:18
  */
-
-public class Sleepwalkers {
-    public static void main(String[] args) {
+public class Sleepwalkers{
+    public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        System.out.println("Input your track number here:");
 
-        switch (in.nextLine()) {
+        System.out.println("input your fav:");
+
+        switch (in.nextLine()){
             case "01":
-                System.out.println("sleepwalkers");
+                System.out.println("foo");
                 break;
             case "02":
-                System.out.println("money for all");
-                break;
-            case "03":
-                System.out.println("ballads of a deadman");
-                break;
-            case "04":
-                System.out.println("angels");
-                break;
-            case "05":
-                System.out.println("world citizen");
-                break;
-            case "06":
-                System.out.println("five lines");
-                break;
-            case "07":
-                System.out.println("the day the earth stolen heaven");
-                break;
-            case "08":
-                System.out.println("playground martyrs");
-                break;
-            case "09":
-                System.out.println("exit/delete");
+                System.out.println("bar");
                 break;
             default:
-                System.out.println("next tracks");
+                System.out.println("chuiniubi");
         }
     }
 }
