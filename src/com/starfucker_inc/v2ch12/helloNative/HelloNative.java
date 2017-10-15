94864f3cc160b00034135cc62ff40bb0f4d4cbf7
zhangjiyun <ayorfree@gmail.com>
2017-10-15 15:47:42 +0800

    do.not.do.that

---
+++ src/com/starfucker_inc/v2ch12/helloNative/HelloNative.java
@@ -0,0 +1,10 @@
+package com.starfucker_inc.v2ch12.helloNative;
+
+/**
+ * @author ayorfree
+ * @create 2017-10-15-下午1:37
+ */
+
+public class HelloNative {
+    public static native void greeting();
+}
