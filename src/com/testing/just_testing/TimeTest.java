75c972a24a0766a3769a5c61ade1b355e866f4ac
zhangjiyun <ayorfree@gmail.com>
2017-10-21 09:27:05 +0800

    maven_t

--- src/com/testing/just_testing/TimeTest.java
+++ src/com/testing/just_testing/TimeTest.java
@@ -1,6 +1,7 @@
 package com.testing.just_testing;
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -21,12 +22,18 @@ public class TimeTest {
         int f = today.getDayOfYear();
         LocalTime t = LocalTime.now();
         DayOfWeek g = today.getDayOfWeek();
+        LocalDateTime dt = LocalDateTime.now();
+
 
         List<Object> dayList = new ArrayList<>();
         dayList.add(today);
         dayList.add(g);
         dayList.add(e);
         dayList.add(t);
+        dayList.add(a);
+        dayList.add(b);
+        dayList.add(c);
+        dayList.add(dt);
 
 
         for (Object o :
